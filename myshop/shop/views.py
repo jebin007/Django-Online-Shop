c1d7b3bf193955410ceeca16aeaa71bb7e4c694a
jebin007 <jevinsubedi@gmail.com>
2017-10-18 15:00:17 -0500

    Creating Shopping Cart Views

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 # Create your views here.
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products =Product.objects.filter(available=True)
+    products = Product.objects.filter(available=True)
     if category_slug:
         category_slug = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -20,10 +21,10 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    #cart_product_form = CartAddProductForm()
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
                   {
                       'product': product,
-                      #'cart_product_form': cart_product_form
+                      'cart_product_form': cart_product_form
                   })
